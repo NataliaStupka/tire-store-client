fe9211627a351d2f60be7f231120003ac8497e67
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-04-23 12:20:57 +0300

    project settings

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Catalog tire</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,45 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./components/App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import "modern-normalize";
+// import { Provider } from "react-redux";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <Provider store={store}> */}
+    <BrowserRouter>
+      <App />
+      {/* Сповіщення */}
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: "",
+          duration: 2000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+
+          // Default options for specific types
+          success: {
+            duration: 4000,
+            theme: {
+              primary: "green",
+              secondary: "black",
+            },
+          },
+        }}
+      />
+    </BrowserRouter>
+    {/* </Provider> */}
+  </StrictMode>
+);
